44b6aca41c57d3f4a3ff3d389df5ee4e7e28f4c3
Marlon Jerold <63025001+MarlonJerold@users.noreply.github.com>
2024-08-27 01:36:06 -0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,7 @@
 
 const CONFIG = {
   // profile setting (required)
+  
   profile: {
     name: "Pato",
     image: "/guts.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
