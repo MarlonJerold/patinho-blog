ec63f9f60b297172b062c10c425d3553555031d0
Marlon Jerold <marlonjerold@outlook.com.br>
2025-01-15 12:06:33 -0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   blog: {
     title: "O blog de um pato",
     description: "pensamentos de um desenvolvedor",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    scheme: "light", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
