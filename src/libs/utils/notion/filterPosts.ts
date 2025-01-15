9e8ccc2df461b9e8fa56fab701a27025a37e6836
Marlon Jerold <marlonjerold@outlook.com.br>
2025-01-15 11:48:38 -0300

    Update filterPosts.ts

--- src/libs/utils/notion/filterPosts.ts
+++ src/libs/utils/notion/filterPosts.ts
@@ -18,23 +18,29 @@ export function filterPosts(
   posts: TPosts,
   options: FilterPostsOptions = initialOption
 ) {
-  const { acceptStatus = ["Public"], acceptType = ["Post"] } = options
+  if (!Array.isArray(posts)) {
+    console.error("Invalid posts data:", posts);
+    return [];
+  }
+
+  const { acceptStatus = ["Public"], acceptType = ["Post"] } = options;
   const filteredPosts = posts
     // filter data
     .filter((post) => {
-      const postDate = new Date(post?.date?.start_date || post.createdTime)
-      if (!post.title || !post.slug || postDate > tomorrow) return false
-      return true
+      const postDate = new Date(post?.date?.start_date || post.createdTime);
+      if (!post.title || !post.slug || postDate > tomorrow) return false;
+      return true;
     })
     // filter status
     .filter((post) => {
-      const postStatus = post.status[0]
-      return acceptStatus.includes(postStatus)
+      const postStatus = post.status?.[0];
+      return postStatus && acceptStatus.includes(postStatus);
     })
     // filter type
     .filter((post) => {
-      const postType = post.type[0]
-      return acceptType.includes(postType)
-    })
-  return filteredPosts
+      const postType = post.type?.[0];
+      return postType && acceptType.includes(postType);
+    });
+
+  return filteredPosts;
 }
